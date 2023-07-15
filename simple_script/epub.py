567cf4df5c710b2dc662c30603fc5ff8fbc00706
Pim <vanhelvoortpim@gmail.com>
2023-07-15 17:24:21 +0200

    chapter select

--- simple_script/epub.py
+++ simple_script/epub.py
@@ -1,19 +1,48 @@
 import ebooklib
 from ebooklib import epub
 
-book = epub.read_epub('./Infinite_Jest__David_Foster_Wallace.epub')
 
-docname = "inf_jest"
+def get_book():
+    book = epub.read_epub('./Infinite_Jest__David_Foster_Wallace.epub')
+    return book
 
 
-print()
-for item in book.get_items():
-    if item.get_type() == ebooklib.ITEM_DOCUMENT:
-        print('==================================')
-        print('NAME : ', item.get_name())
-        print('----------------------------------')
-        # print(item.get_content())
-        # with open(f"./{docname}/{item.get_name().split('/')[-1]}", "r") as f:
-        #     print(f.read())
-        #     # f.write(item.get_content())
-        print('==================================')
+# docname = "inf_jest"
+
+
+def select_chapter(book_obj):
+    print("content:")
+    i = 0
+    content_dict = {}
+
+    # for item in book_obj.get_items():
+    #     if item.get_type() == ebooklib.ITEM_DOCUMENT:
+    for item in book_obj.toc:
+        i += 1
+        print(f"{i}: {item.title}")
+        content_dict[str(i)] = item.title
+
+        # print('----------------------------------')
+    while (chapter_index := input("select a chapter: ")) not in content_dict.keys():
+        chapter_index = input("select a chapter: ")
+    return content_dict[chapter_index]
+    # print(item.get_content())
+    # with open(f"./{docname}/{item.get_name().split('/')[-1]}", "r") as f:
+    #     print(f.read())
+    #     # f.write(item.get_content())
+
+
+def open_chapter(book_obj, chapter_title):
+    chapter = book_obj.get_item_with_nav_label(chapter_title)
+    content = chapter.content
+    print(str(content))
+
+
+def main():
+    book_obj = get_book()
+    chapter = select_chapter(book_obj)
+    print(book_obj, chapter)
+
+
+if __name__ == "__main__":
+    main()
